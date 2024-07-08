b7761c2f602d25e5fd05b0a69355caada88cc11d
Kevin Marrec <kevin@marrec.io>
2024-07-08 19:59:29 +0200

    chore: prepare script performance check

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -4,11 +4,15 @@ import { $ } from 'bun'
 const packages = [
   'eslint-config',
   'stylelint-config',
-  // 'frontend',
+  'frontend',
 ]
+
+const start = performance.now()
 
 await Promise.all(
   packages.map(pkg =>
     $`bun --cwd packages/${pkg} build`.quiet(),
   ),
 )
+
+console.log('Pre-build took', (performance.now() - start).toFixed(2), 'ms')
