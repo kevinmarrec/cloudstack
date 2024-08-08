81a9161851c5241ca0f759e830cd123c9add3234
Kevin Marrec <kevin@marrec.io>
2024-08-08 23:13:39 +0200

    chore: adjust tsup.config.ts files

--- packages/eslint-config/tsup.config.ts
+++ packages/eslint-config/tsup.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig(
-  {
-    clean: true,
-    dts: true,
-    entry: ['src/index.ts'],
-    format: 'esm',
-  },
-)
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ['src/index.ts'],
+  format: 'esm',
+})
--- packages/unocss-preset/tsup.config.ts
+++ packages/unocss-preset/tsup.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig(
-  {
-    clean: true,
-    dts: true,
-    entry: ['src/index.ts'],
-    external: [/unocss/],
-    format: 'esm',
-  },
-)
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ['src/index.ts'],
+  format: 'esm',
+})
