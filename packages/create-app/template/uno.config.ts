1cceb55b3b9d2bfc9b0bbe022c20057dc86cbfa4
Kevin Marrec <kevin@marrec.io>
2024-09-22 18:06:43 +0200

    chore(create-app): simplify config files

--- packages/create-app/template/uno.config.ts
+++ packages/create-app/template/uno.config.ts
@@ -2,5 +2,5 @@ import preset from '@kevinmarrec/cloudstack-unocss-preset'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-  presets: [preset()],
+  presets: [preset],
 })
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,7 +2,5 @@ import Cloudstack from '@kevinmarrec/cloudstack-vite-plugin'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [
-    Cloudstack({}),
-  ],
+  plugins: [Cloudstack()],
 })
