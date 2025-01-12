d18a7e3b901e48bc2c334a9610c64fd3e5472ad9
Kevin Marrec <kevin@marrec.io>
2025-01-13 00:07:03 +0100

    feat(create-app): show creation duration

--- packages/create-app/src/run.ts
+++ packages/create-app/src/run.ts
@@ -1,9 +1,6 @@
 /* eslint-disable no-console */
 
-import path from 'node:path'
-import process from 'node:process'
-
-import { bold, green } from 'picocolors'
+import { blue, bold } from 'picocolors'
 import { x } from 'tinyexec'
 
 import { prompt } from './prompt'
@@ -12,21 +9,14 @@ import { scaffold } from './scaffold'
 export async function run() {
   const { targetDir, install } = await prompt()
 
+  const time = Date.now()
+
   await scaffold(targetDir)
 
   if (install) {
     console.log('\n📦 Installing dependencies...')
     await x('bun', ['install', '--cwd', targetDir])
-    console.log('\n✅ Done')
   }
-  else {
-    console.log('\n✅ Done')
-    console.log(`\n👉 Next steps:\n`)
 
-    if (targetDir !== process.cwd()) {
-      console.log(bold(green(`    cd ${path.relative(process.cwd(), targetDir)}`)))
-    }
-
-    console.log(bold(green('    bun install --save-text-lockfile')))
-  }
+  console.log('\n✅ Done in', bold(blue(`${((Date.now() - time) / 1000).toFixed(2)}s`)))
 }
