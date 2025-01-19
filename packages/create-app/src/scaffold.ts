cd9e4fab4c8f0e4a2ee222e64d9bc090cb4c0a0d
Kevin Marrec <kevin@marrec.io>
2025-01-19 14:57:15 +0100

    fix(create-app): replace `__dirname` by `import.meta.dirname`

--- packages/create-app/src/scaffold.ts
+++ packages/create-app/src/scaffold.ts
@@ -16,7 +16,7 @@ export async function scaffold(root: string) {
   console.log(`\n🧬 Scaffolding project in ${colors.blue(root)}...`)
 
   // Copy template
-  await fs.cp(path.join(__dirname, '../template'), root, { recursive: true })
+  await fs.cp(path.join(import.meta.dirname, '../template'), root, { recursive: true })
   await fs.rename(path.join(root, 'gitignore'), path.join(root, '.gitignore'))
 
   // Set @kevinmarrec-cloudstack-* packages version to current version
