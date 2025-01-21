d48633458c424fa0b638e7ab32ec337e34109873
Kevin Marrec <kevin@marrec.io>
2025-01-21 22:35:49 +0100

    chore(vite-plugin): simplify vue integration

--- packages/vite-plugin/src/integrations/vue.ts
+++ packages/vite-plugin/src/integrations/vue.ts
@@ -1,5 +1,3 @@
-import process from 'node:process'
-
 import Vue from '@vitejs/plugin-vue'
 
 import { integrationFactory } from './_factory'
@@ -8,8 +6,7 @@ export default integrationFactory(Vue, {
   options: ctx => ctx.userOptions.vue,
   defaults: () => ({
     features: {
-      // Drop Options API support in production build to optimize bundle size
-      optionsAPI: process.env.NODE_ENV !== 'production',
+      optionsAPI: false,
     },
   }),
 })
