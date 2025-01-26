5f955ff0b7600174ecaa69e382c6cbcf7657055d
Kevin Marrec <kevin@marrec.io>
2025-01-26 15:01:38 +0100

    chore: better scripts

--- scripts/prepublish.ts
+++ scripts/prepublish.ts
@@ -2,8 +2,12 @@ import fs from 'node:fs/promises'
 
 import { glob } from 'tinyglobby'
 
+import { version } from '../package.json'
+
 const pkgPaths = await glob('packages/*', { onlyDirectories: true })
 
 await Promise.all(pkgPaths.map(async (pkgPath) => {
+  const pkgJson = await fs.readFile(`${pkgPath}/package.json`, 'utf-8')
+  await fs.writeFile(`${pkgPath}/package.json`, pkgJson.replace('workspace:*', `^${version}`))
   await fs.cp('LICENSE', `${pkgPath}/LICENSE`)
 }))
