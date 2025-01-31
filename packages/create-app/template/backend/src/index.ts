ef953734a79b2d6c364dd9e322f36c07ece8e191
Kevin Marrec <kevin@marrec.io>
2025-01-31 19:02:28 +0100

    chore: add `playground` script

--- packages/create-app/template/backend/src/index.ts
+++ packages/create-app/template/backend/src/index.ts
@@ -1,5 +1,9 @@
-Bun.serve({
+const server = Bun.serve({
   fetch: () => {
     return new Response('Hello World')
   },
 })
+
+// TODO: Remove this console.log by adding proper logger
+// eslint-disable-next-line no-console
+console.log(`Listening on ${server.url}`)
