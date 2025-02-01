58706b4ad0083401b67626d3613a521c49330972
Kevin Marrec <kevin@marrec.io>
2025-02-01 23:58:21 +0100

    chore: improve `bump.config.ts`

--- bump.config.ts
+++ bump.config.ts
@@ -2,18 +2,20 @@ import fs from 'node:fs/promises'
 
 import { defineConfig } from 'bumpp'
 
+async function bumpLockfile(newVersion: string) {
+  const lockFile = await fs.readFile('bun.lock', 'utf-8')
+  await fs.writeFile('bun.lock', lockFile.replace(
+    /"version": "(.*?)"/g,
+    `"version": "${newVersion}"`,
+  ))
+}
+
 export default defineConfig({
   all: true,
-  execute: async ({ state: { newVersion } }) => {
-    const lockFile = await fs.readFile('bun.lock', 'utf-8')
-    await fs.writeFile('bun.lock', lockFile.replace(
-      /"version": "(.*?)"/g,
-      `"version": "${newVersion}"`,
-    ))
-  },
   files: [
     'package.json',
     'packages/**/package.json',
     '!**/template/**',
   ],
+  execute: ({ state: { newVersion } }) => bumpLockfile(newVersion),
 })
