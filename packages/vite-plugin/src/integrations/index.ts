77ba938baa6e9af70b702517c83a1ae95aeaf46c
Kevin Marrec <kevin@marrec.io>
2025-02-01 20:32:19 +0100

    feat(vite-plugin): add `tsconfig-paths` integration

--- packages/vite-plugin/src/integrations/index.ts
+++ packages/vite-plugin/src/integrations/index.ts
@@ -1,5 +1,6 @@
 import cloudstack from './cloudstack'
 import pwa from './pwa'
+import tsconfigPaths from './tsconfig-paths'
 import unocss from './unocss'
 import vue from './vue'
 import vueDevTools from './vue-devtools'
@@ -7,6 +8,7 @@ import vueDevTools from './vue-devtools'
 export default [
   ...cloudstack,
   pwa,
+  tsconfigPaths,
   unocss,
   vue,
   vueDevTools,
---
+++ packages/vite-plugin/src/integrations/tsconfig-paths.ts
@@ -0,0 +1,5 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { integrationFactory } from './_factory'
+
+export default integrationFactory(tsconfigPaths)
