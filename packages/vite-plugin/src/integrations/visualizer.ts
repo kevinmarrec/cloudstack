2c20c328f8efecb1dd50a945b6f033a49491ddc5
Kevin Marrec <kevin@marrec.io>
2025-02-09 00:26:00 +0100

    chore: up

--- packages/vite-plugin/src/integrations/visualizer.ts
+++ packages/vite-plugin/src/integrations/visualizer.ts
@@ -3,7 +3,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 import { integrationFactory } from './_factory'
 
 export default integrationFactory(visualizer, {
-  enabled: ctx => ctx.env?.command === 'build' && ctx.env?.mode === 'analyze',
+  enabled: ctx => ctx.env?.command === 'build' && ctx.env?.mode === 'analyze' && !ctx.env?.isSsrBuild,
   options: ctx => ctx.userOptions.visualizer,
   defaults: () => ({
     filename: 'node_modules/.cache/cloudstack/stats.html',
