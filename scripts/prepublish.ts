5efe9f3603d470da97d715a800fedeea01dcad82
Kevin Marrec <kevin@marrec.io>
2025-03-03 10:45:46 +0100

    chore(scripts): scripts enhancements

--- scripts/prepublish.ts
+++ scripts/prepublish.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises'
 
+import { join } from 'pathe'
 import { glob } from 'tinyglobby'
 
 import { version } from '../package.json'
@@ -7,7 +8,7 @@ import { version } from '../package.json'
 const pkgPaths = await glob('packages/*', { onlyDirectories: true })
 
 await Promise.all(pkgPaths.map(async (pkgPath) => {
-  const pkgJson = await fs.readFile(`${pkgPath}/package.json`, 'utf-8')
-  await fs.writeFile(`${pkgPath}/package.json`, pkgJson.replaceAll('workspace:*', `^${version}`))
-  await fs.cp('LICENSE', `${pkgPath}/LICENSE`)
+  const pkgJson = await fs.readFile(join(pkgPath, 'package.json'), 'utf-8')
+  await fs.writeFile(join(pkgPath, 'package.json'), pkgJson.replaceAll('workspace:*', `^${version}`))
+  await fs.cp('LICENSE', join(pkgPath, 'LICENSE'))
 }))
