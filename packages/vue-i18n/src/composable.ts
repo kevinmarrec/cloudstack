9f73b14c442daf4d9c7fa916ecd362e10dd30b33
Kevin Marrec <kevin@marrec.io>
2025-03-05 00:57:04 +0100

    chore: change error message

--- packages/vue-i18n/src/composable.ts
+++ packages/vue-i18n/src/composable.ts
@@ -6,7 +6,7 @@ export function useI18n() {
   const instance = inject(injectionKey)
 
   if (!instance)
-    throw new Error('No Vue I18n instance found')
+    throw new Error('Plugin has not been installed')
 
   return instance
 }
