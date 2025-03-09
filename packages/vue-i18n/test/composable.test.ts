b4d312c376fcce55505d4b170c280810180664ff
Kevin Marrec <kevin@marrec.io>
2025-03-09 01:07:38 +0100

    test(vue-i18n): implement missing tests

--- packages/vue-i18n/test/composable.test.ts
+++ packages/vue-i18n/test/composable.test.ts
@@ -3,17 +3,113 @@
  */
 
 import { mount } from '@vue/test-utils'
-import { describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { defineComponent } from 'vue'
 
 import { createI18n, useI18n } from '../src'
 
+afterEach(() => {
+  vi.unstubAllGlobals()
+})
+
 describe('composable', () => {
-  it('should setup a default locale', async () => {
+  it('should translate (default locale)', async () => {
     const Component = defineComponent({
       setup() {
-        const { locale } = useI18n()
-        return () => locale.value
+        const { t } = useI18n()
+        return () => t('welcome')
+      },
+    })
+
+    const wrapper = mount(Component, {
+      global: {
+        plugins: [
+          await createI18n({
+            messages: {
+              './locales/en.json': () => Promise.resolve({
+                default: {
+                  welcome: 'Welcome !',
+                },
+              }),
+            },
+          }),
+        ],
+      },
+    })
+
+    await vi.waitFor(() => {
+      expect(wrapper.text()).toBe('Welcome !')
+    })
+  })
+
+  it('should translate (navigator language)', async () => {
+    vi.stubGlobal('navigator', {
+      language: 'fr-FR',
+    })
+
+    const Component = defineComponent({
+      setup() {
+        const { t } = useI18n()
+        return () => t('welcome')
+      },
+    })
+
+    const wrapper = mount(Component, {
+      global: {
+        plugins: [
+          await createI18n({
+            messages: {
+              en: {
+                welcome: 'Welcome!',
+              },
+              fr: {
+                welcome: 'Bienvenue !',
+              },
+            },
+          }),
+        ],
+      },
+    })
+
+    await vi.waitFor(() => {
+      expect(wrapper.text()).toBe('Bienvenue !')
+    })
+  })
+
+  it('should translate (fallback locale when translation not found)', async () => {
+    vi.stubGlobal('navigator', {
+      language: 'fr-FR',
+    })
+
+    const Component = defineComponent({
+      setup() {
+        const { t } = useI18n()
+        return () => t('welcome')
+      },
+    })
+
+    const wrapper = mount(Component, {
+      global: {
+        plugins: [
+          await createI18n({
+            messages: {
+              en: {
+                welcome: 'Welcome!',
+              },
+            },
+          }),
+        ],
+      },
+    })
+
+    expect(wrapper.text()).toBe('Welcome!')
+  })
+
+  it('should translate (raw key when fallback translation also not found)', async () => {
+    const Component = defineComponent({
+      setup() {
+        const { t } = useI18n()
+        return () => t('welcome')
       },
     })
 
@@ -25,6 +121,16 @@ describe('composable', () => {
       },
     })
 
-    expect(wrapper.text()).toBe('en')
+    expect(wrapper.text()).toBe('welcome')
+  })
+
+  it('should throw error when plugin has not been installed', () => {
+    const Component = defineComponent({
+      setup() {
+        useI18n()
+      },
+    })
+
+    expect(() => mount(Component)).toThrowError('Plugin has not been installed')
   })
 })
