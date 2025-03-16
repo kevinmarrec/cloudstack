e44d4a5bafabc75dd47f49f611f4e17be2f007d0
Kevin Marrec <kevin@marrec.io>
2025-03-16 15:16:17 +0100

    chore: update

--- packages/create-app/template/frontend/src/main.ts
+++ packages/create-app/template/frontend/src/main.ts
@@ -1,5 +1,7 @@
 import { createI18n } from '@kevinmarrec/cloudstack-vue-i18n'
 import { Cloudstack } from 'virtual:cloudstack'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
+import { watchEffect } from 'vue'
 
 import App from './App.vue'
 
@@ -7,4 +9,13 @@ export const createApp = Cloudstack(App, async ({ app }) => {
   app.use(await createI18n({
     messages: import.meta.glob('./locales/*.yml'),
   }))
+
+  const { updateServiceWorker, needRefresh } = useRegisterSW()
+
+  watchEffect(() => {
+    if (needRefresh.value) {
+      // eslint-disable-next-line no-alert
+      confirm('New content available, refresh?') && updateServiceWorker()
+    }
+  })
 })
--- packages/vite-plugin/src/integrations/pwa.ts
+++ packages/vite-plugin/src/integrations/pwa.ts
@@ -1,3 +1,7 @@
+import { createHash } from 'node:crypto'
+import { readFileSync } from 'node:fs'
+
+import { join } from 'pathe'
 import { type DisplayOverride, type LaunchHandlerClientMode, VitePWA } from 'vite-plugin-pwa'
 
 import { integrationFactory } from './_factory'
@@ -5,45 +9,59 @@ import { integrationFactory } from './_factory'
 export default integrationFactory(VitePWA, {
   enabled: ({ userOptions }) => userOptions.pwa !== false,
   options: ({ userOptions }) => userOptions.pwa,
-  defaults: ({ env }) => ({
-    manifest: {
-      id: '/',
-      scope: '/',
-      start_url: '/',
-      lang: 'en',
-      name: 'Cloudstack',
-      short_name: 'Cloudstack',
-      background_color: '#ffffff',
-      theme_color: '#ffffff',
-      dir: 'ltr',
-      orientation: 'natural',
-      display: 'standalone',
-      display_override: [
-        'window-controls-overlay',
-      ] as DisplayOverride[],
-      handle_links: 'preferred',
-      launch_handler: {
-        client_mode: [
-          'navigate-existing',
-          'auto',
-        ] as LaunchHandlerClientMode[],
+  defaults: ({ env, userOptions }) => {
+    const pwaImage = join(
+      (typeof userOptions.pwa === 'object' && userOptions.pwa.pwaAssets?.image) || 'public/favicon.svg',
+    )
+
+    const faviconHash = createHash('md5')
+      .update(readFileSync(pwaImage, 'utf-8'))
+      .digest('hex')
+
+    return {
+      manifest: {
+        id: '/',
+        scope: '/',
+        start_url: '/',
+        lang: 'en',
+        name: 'Cloudstack',
+        short_name: 'Cloudstack',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        dir: 'ltr',
+        orientation: 'natural',
+        display: 'standalone',
+        display_override: [
+          'window-controls-overlay',
+        ] as DisplayOverride[],
+        handle_links: 'preferred',
+        launch_handler: {
+          client_mode: [
+            'navigate-existing',
+            'auto',
+          ] as LaunchHandlerClientMode[],
+        },
       },
-    },
-    injectRegister: 'script-defer',
-    registerType: 'autoUpdate',
-    pwaAssets: {
-      overrideManifestIcons: true,
-    },
-    workbox: {
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-      skipWaiting: true,
-    },
-    ...env?.mode === 'development' && {
-      selfDestroying: true,
-      devOptions: {
-        enabled: true,
+      pwaAssets: {
+        overrideManifestIcons: true,
       },
-    },
-  } as const),
+      workbox: {
+        additionalManifestEntries: [
+          'favicon.ico',
+          'favicon.svg',
+          'apple-touch-icon-180x180.png',
+          'pwa-64x64.png',
+          'pwa-192x192.png',
+          'pwa-512x512.png',
+          'maskable-icon-512x512.png',
+        ].map(url => ({ url, revision: faviconHash })),
+      },
+      ...env?.mode === 'development' && {
+        selfDestroying: true,
+        devOptions: {
+          enabled: true,
+        },
+      },
+    } as const
+  },
 })
