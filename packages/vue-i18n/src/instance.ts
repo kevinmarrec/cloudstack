79f439b378d6952a6542fd018a56102f487585cb
Kevin Marrec <kevin@marrec.io>
2025-03-21 15:44:07 +0100

    refactor(vue-i18n): better types

--- packages/vue-i18n/src/instance.ts
+++ packages/vue-i18n/src/instance.ts
@@ -1,22 +1,22 @@
 import { readonly, ref, watch } from 'vue'
 
-import type { LocaleMessages, Messages, ResolvedVueI18nOptions } from './types'
+import type { LocaleMessageDictionary, LocaleMessages, LocaleMessageValue, ResolvedVueI18nOptions } from './types'
 
 export function createInstance(options: ResolvedVueI18nOptions) {
   const availableLocales = Object.keys(options.messages)
   const locale = ref(options.locale)
   const fallbackLocale = ref(options.fallbackLocale)
-  const messages = ref<Messages>({})
+  const messages = ref<LocaleMessages>({})
 
-  async function loadMessages(locale: string) {
+  const loadMessages = async (locale: string) => {
     messages.value[locale] = typeof options.messages[locale] === 'function'
       ? (await options.messages[locale]() as any).default
       : options.messages[locale]
   }
 
-  function findLocaleMessage(locale: string, key: string) {
-    return key.split('.').reduce((path, segment) => path?.[segment] as LocaleMessages, messages.value[locale])
-  }
+  const findLocaleMessage = (locale: string, key: string) =>
+    key.split('.').reduce<LocaleMessageValue | undefined>((path, segment) =>
+      (path as LocaleMessageDictionary | undefined)?.[segment], messages.value[locale])
 
   const isReady = new Promise<void>((resolve) => {
     loadMessages(locale.value)
@@ -32,10 +32,11 @@ export function createInstance(options: ResolvedVueI18nOptions) {
     fallbackLocale,
     messages: readonly(messages),
     isReady: () => isReady,
-    t: (key: string) =>
-      findLocaleMessage(locale.value, key)
-      ?? findLocaleMessage(fallbackLocale.value, key)
-      ?? key,
+    t: (key: string) => {
+      return findLocaleMessage(locale.value, key)
+        ?? findLocaleMessage(fallbackLocale.value, key)
+        ?? key
+    },
   }
 }
 
--- packages/vue-i18n/src/types.ts
+++ packages/vue-i18n/src/types.ts
@@ -1,19 +1,21 @@
+export type LocaleMessageValue = LocaleMessageDictionary | string
+
+export interface LocaleMessageDictionary {
+  [key: string]: LocaleMessageValue
+}
+
 export interface LocaleMessages {
-  [key: string]: string | LocaleMessages
+  [locale: string]: LocaleMessageDictionary
 }
 
-export interface Messages {
-  [locale: string]: LocaleMessages
-}
-
-interface LazyMessages {
-  [path: string]: () => Promise<unknown>
+interface LazyLocaleMessages {
+  [localePath: string]: () => Promise<unknown>
 }
 
 export interface VueI18nOptions {
   locale?: string
   fallbackLocale?: string
-  messages?: Messages | LazyMessages
+  messages?: LocaleMessages | LazyLocaleMessages
 }
 
 export interface ResolvedVueI18nOptions extends Required<VueI18nOptions> {}
