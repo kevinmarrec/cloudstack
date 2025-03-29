d2057d115ebe62534f09c9f1acc2f45a5bc3d4a4
Kevin Marrec <kevin@marrec.io>
2025-03-29 23:54:14 +0100

    chore(deps): update dependencies 🚀

--- packages/create-app/template/frontend/src/App.vue
+++ packages/create-app/template/frontend/src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { useHead } from '@kevinmarrec/cloudstack-vue/head'
 import { useI18n } from '@kevinmarrec/cloudstack-vue/i18n'
-import { computed } from 'vue'
 
 const { t } = useI18n()
 
 useHead({
-  title: computed(() => t('title')),
+  title: () => t('title'),
 })
 </script>
 
