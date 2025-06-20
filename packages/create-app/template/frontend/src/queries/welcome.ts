634687d3ab37ce2748098252d6c9b2f9509c8517
Kevin Marrec <kevin@marrec.io>
2025-06-20 20:01:33 +0200

    chore: light update

--- packages/create-app/template/frontend/src/queries/welcome.ts
+++ packages/create-app/template/frontend/src/queries/welcome.ts
@@ -1,9 +1,10 @@
 import { orpc } from '@frontend/lib/orpc'
+import { get } from '@frontend/utils'
 import { useQuery } from '@tanstack/vue-query'
-import { computed, type MaybeRef, unref } from 'vue'
+import { computed, type MaybeRef } from 'vue'
 
 export function useWelcome(text: MaybeRef<string>) {
   return useQuery(computed(() => orpc.welcome.queryOptions({
-    input: unref(text),
+    input: get(text),
   })))
 }
