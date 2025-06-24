537b22ff0e8b9f41badd9b615888cc78accc4c70
Kevin Marrec <kevin@marrec.io>
2025-06-24 17:22:12 +0200

    chore: up

--- packages/create-app/template/backend/src/database/client.ts
+++ packages/create-app/template/backend/src/database/client.ts
@@ -1,9 +1,5 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { drizzle } from 'drizzle-orm/bun-sql'
 
 import { schema } from './schema'
-
-if (import.meta.env.NODE_ENV !== 'development') {
-  throw new Error('Database client is only available in development mode')
-}
 
 export const client = drizzle(import.meta.env.DATABASE_URL, { schema })
